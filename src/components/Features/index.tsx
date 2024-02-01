b587a4555c1a17ac60a117e205da89ba328b827e
subhash131 <subhashnayak131@gmail.com>
2024-02-01 23:01:29 +0530

    create component: Features

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div className="w-screen h-screen ">
+      <div className="flex overflow-scroll w-screen  h-full bg-green-100 gap-10">
+        <div className="w-[500px] bg-red-100 whitespace-nowrap">
+          a Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
