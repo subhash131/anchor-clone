11bf742a3013fa83314673bfb98179eac657f62a
subhash131 <subhashnayak131@gmail.com>
2024-02-14 02:18:56 +0530

    update styles

--- src/components/Services/Card.tsx
+++ src/components/Services/Card.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Card = ({ title }: { title: string }) => {
-  return <div className="w-[80vw] h-[80%] bg-red-200 shrink-0">{title}</div>;
+  return (
+    <div className="w-[80vw] h-[80%] bg-red-200 shrink-0 rounded-2xl">
+      {title}
+    </div>
+  );
 };
 
 export default Card;
