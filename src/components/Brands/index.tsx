cc4666038ae8108e1772c841c00f7ddfa250b765
subhash131 <subhashnayak131@gmail.com>
2024-02-14 19:26:28 +0530

    update styles: Brands

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -5,10 +5,9 @@ import List from "./List";
 const Brands = () => {
   return (
     <div
-      className={`overflow-hidden relative w-full h-24 before:bg-gradient-linear-to-r before:h-full before:w-[5%] before:z-10 before:absolute before:-right-1 before:top-0 after:absolute after:w-[5%] after:h-full after:bg-gradient-linear-to-l after:z-10 after:top-0 after:left-0 whitespace-nowrap flex items-center ${styles.slide_wrap} `}
+      className={`overflow-hidden relative w-full h-24 before:bg-gradient-linear-to-r before:h-full before:w-[5%] before:z-10 before:absolute before:-right-1 before:top-0 after:absolute after:w-[5%] after:h-full after:bg-gradient-linear-to-l after:z-10 after:top-0 after:left-0 whitespace-nowrap flex items-center px-20 ${styles.slide_wrap} `}
     >
       <List />
-      {/* Copy of same items */}
       <List />
     </div>
   );
