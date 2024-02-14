cdfffc92d936b88995880ef40ac1d2029176a73a
subhash131 <subhashnayak131@gmail.com>
2024-02-14 14:35:09 +0530

    add interactive buttons

--- src/components/Services/SwiperButtons.tsx
+++ src/components/Services/SwiperButtons.tsx
@@ -1,10 +1,27 @@
 "use client";
-import React from "react";
+import React, { useContext } from "react";
+import { ServiceCardContext } from "./provider";
 
 const SwiperButtons = () => {
+  const serviceContext = useContext(ServiceCardContext);
+  const handleRightArrowClick = () => {
+    if (serviceContext?.cardIndex! >= 0 && serviceContext?.cardIndex! < 3) {
+      serviceContext?.setCardIndex((preVal) => preVal + 1);
+    }
+  };
+  const handleLeftArrowClick = () => {
+    if (serviceContext?.cardIndex! > 0) {
+      serviceContext?.setCardIndex((preVal) => preVal - 1);
+    }
+  };
   return (
     <div className="flex h-fit gap-4 ">
-      <button className="p-6 rounded-lg bg-[#F8F8F8]" onClick={() => {}}>
+      <button
+        className={`p-6 rounded-lg ${
+          serviceContext?.cardIndex === 0 ? "bg-[#F8F8F8]" : "bg-[#F1F9F6]"
+        } `}
+        onClick={handleLeftArrowClick}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -19,7 +36,12 @@ const SwiperButtons = () => {
           <path d="m15 18-6-6 6-6" />
         </svg>
       </button>
-      <button className="p-6 rounded-lg bg-[#F1F9F6]" onClick={() => {}}>
+      <button
+        className={`p-6 rounded-lg ${
+          serviceContext?.cardIndex === 3 ? "bg-[#F8F8F8]" : "bg-[#F1F9F6]"
+        } `}
+        onClick={handleRightArrowClick}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
--- src/components/Services/SwiperComponent.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import React, { useRef, useState } from "react";
-
-export default function SwiperComponent() {
-  return <></>;
-}
