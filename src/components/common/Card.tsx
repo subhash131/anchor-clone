862287761056b1a3e5b4825e5ef9cfcab69fcd55
subhash131 <subhashnayak131@gmail.com>
2024-02-14 19:26:47 +0530

    add motion to card

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,16 +1,29 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+
 export type CardType = {
   children: string;
   className: string;
+  top: number;
+  initialTop: number;
+  delay: number;
 };
 
-const Card = ({ children, className }: CardType) => {
+const Card = ({ children, className, initialTop, top, delay }: CardType) => {
   return (
-    <div
+    <motion.div
       className={`w-80 h-20 flex items-center justify-start px-10 rounded-xl absolute ${className}`}
+      initial={{
+        top: initialTop,
+      }}
+      animate={{
+        top,
+      }}
+      transition={{ type: "spring", stiffness: 100, delay }}
     >
       <p>{children}</p>
-    </div>
+    </motion.div>
   );
 };
 
